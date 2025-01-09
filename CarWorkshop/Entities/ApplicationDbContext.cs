8f27fd505c8160d114e76c2b8f19fb9d905c9f68
Michal Rawicz <michal.rawicz@euvic.pl>
2025-01-09 19:59:39 +0100

    Add base of my prject, particular ready.

---
+++ CarWorkshop/Entities/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarWorkshop.Entities 
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<CarWorkshopEntity> CarWorkshops { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CarWorkshop;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
