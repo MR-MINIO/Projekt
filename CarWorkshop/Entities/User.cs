f9d2fac437c78b20c01c98dfc7205dd08dcebc36
Michal Rawicz <michal.rawicz@euvic.pl>
2025-01-10 19:19:02 +0100

    remove unnesesary files

--- CarWorkshop/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CarWorkshop.Entities
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-    }
-}
